f5d6b807b06b03bc1b73c0a29a2485b1ca96a598
mukesh-s-20 <mukeshselva144@gmail.com>
2026-04-07 22:45:51 +0530

    Fix future import order

--- server/app.py
+++ server/app.py
@@ -1,1 +1,13 @@
 from main import app
+import uvicorn
+import os
+
+
+def main():
+    host = os.getenv("HOST", "0.0.0.0")
+    port = int(os.getenv("PORT", "7860"))
+    uvicorn.run(app, host=host, port=port)
+
+
+if __name__ == "__main__":
+    main()
